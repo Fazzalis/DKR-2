09b195fbd76a1c77fc818ba2a5f59dda273f7892
Fazzalis <117628490+Fazzalis@users.noreply.github.com>
2022-12-01 12:15:21 +0300

    Add files via upload

---
+++ DKR2_1.pas
@@ -0,0 +1,37 @@
+﻿begin
+  var l := ReadInteger('введите порядковый номер последнего элемента массива, начиная с 0:');
+  var A: array of integer; 
+  
+  randomize;
+  for var i: integer := 0 to l do
+  begin
+    setlength(A, i + 1);
+    a[i] := random(-100, 100);
+  end;
+  for var n: integer := 0 to l do
+  begin
+    print('A[', n, '] = ');
+    println(A[n]);
+  end;
+  var min := A[1];
+  var kolvo := 0;
+  var odin := 1;
+  for var k: integer := 0 to l do
+  begin
+    if A[k] < min
+    then 
+    begin
+      min := A[k];
+      kolvo += 0;
+    end;
+  end;
+  kolvo += 0;
+  for var m: integer := 0 to l do
+  begin
+    if min = A[m]
+      then kolvo += odin
+    else kolvo += 0;
+  end;
+  print('мин эл массива = ', min);
+  print('; его вхождение в массив = ', kolvo);
+end.
---
+++ DKR2_2.pas
@@ -0,0 +1,14 @@
+﻿begin
+  var A:= ReadString('введите строку: ');
+  var n: integer;
+  var bukvi:=0; //ytghfdbkmyj
+  var slova:=0;
+  
+  n:=length(A);
+  for var i: integer:=1 to n do
+  begin
+    if (A[i]=' ') or (A[i]='.') or (A[i]=',') or (A[i]=';') or (A[i]='!') then slova+=1
+    else begin bukvi+=1; if (i=n) then slova+=1; end;
+  end;
+  print('кол-во слов = ',slova,' кол-во букв = ',bukvi);
+end.
